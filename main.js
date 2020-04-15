c9214de42dd5629be3a43a0e203ff51c51a7db3c
dol.jpo <dol.jpo@hotmail.com>
2020-04-14 21:43:41 -0300

    Implementada função para obter fotos aleatórias

--- main.js
+++ main.js
@@ -2,29 +2,29 @@ const app = new Vue({
     el: '#app',
     data: {
         title: 'Vue Random Gallery',
-        numberOfPhotos: 4,
-        photos: []
+        numberOfPhotos: 8,
+        photos: [],
+        listOfPhotos: []
     },
 
     created() {
-        
-        this.photos.push({ url: './photos/photo01.jpg', alt: 'Photo by Alan Quirvan on Unsplash' });
-        this.photos.push({ url: './photos/photo02.jpg', alt: 'Photo by roya ann miller on Unsplash' });
-        this.photos.push({ url: './photos/photo03.jpg', alt: 'Photo by v2osk on Unsplash' });
-        this.photos.push({ url: './photos/photo04.jpg', alt: 'Photo by LOGAN WEAVER on Unsplash' });
-        this.photos.push({ url: './photos/photo05.jpg', alt: 'Photo by Jonathan Cooper on Unsplash' });
-        this.photos.push({ url: './photos/photo06.jpg', alt: 'Photo by Annie Spratt on Unsplash' });
-        this.photos.push({ url: './photos/photo07.jpg', alt: 'Photo by Inés Castellano on Unsplash' });
-        this.photos.push({ url: './photos/photo08.jpg', alt: 'Photo by Imraumanzug on Unsplash' });
+        for (let index = 0; index < this.numberOfPhotos; index++) {
+            this.photos.push({ id: index, url: '', alt: '' });
+        }
 
-        // for (let index = 1; index <= this.numberOfPhotos; index++) {
-        //     this.photos.push({ url: 'https://picsum.photos/200/200?random=' + index });
-        // }
+        this.getNewListOfPhotos();
     },
 
     methods: {
-        atualizar() {
-            location.reload();
+        getNewListOfPhotos() {
+            fetch('https://picsum.photos/v2/list?limit=' + this.numberOfPhotos + '&page=' + ((Math.round(Math.random() * 123)) + 1))
+            .then(response => response.json())
+            .then(photos => {
+                this.photos.forEach(photo => {
+                    photo.url = 'https://picsum.photos/id/' + photos[photo.id].id + '/200/200';
+                    photo.title = photos[photo.id].author;
+                });
+            });
         }
     }
 });
