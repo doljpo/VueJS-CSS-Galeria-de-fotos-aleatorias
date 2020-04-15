2b768b5569bbd9ee35ab2eb3e85a5208d09c7bd8
dol.jpo <dol.jpo@hotmail.com>
2020-04-15 08:59:02 -0300

    Adicionado template para novas funcionalidades on hover

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const app = new Vue({
 
     created() {
         for (let index = 0; index < this.numberOfPhotos; index++) {
-            this.photos.push({ id: index, unsplashUrl: '', url: '', alt: '' });
+            this.photos.push({ id: index });
         }
 
         this.getNewListOfPhotos();
@@ -17,15 +17,21 @@ const app = new Vue({
 
     methods: {
         getNewListOfPhotos() {
-            fetch('https://picsum.photos/v2/list?limit=' + this.numberOfPhotos + '&page=' + ((Math.round(Math.random() * 123)) + 1))
-            .then(response => response.json())
-            .then(photos => {
-                this.photos.forEach(photo => {
-                    photo.url = 'https://picsum.photos/id/' + photos[photo.id].id + '/350/250';
-                    photo.author = photos[photo.id].author;
-                    photo.unsplashUrl = photos[photo.id].url;
-                });
-            });
+            // fetch('https://picsum.photos/v2/list?limit=' + this.numberOfPhotos + '&page=' + ((Math.round(Math.random() * 123)) + 1))
+            // .then(response => response.json())
+            // .then(photos => {
+            //     this.photos.forEach(photo => {
+            //         photo.src = 'https://picsum.photos/id/' + photos[photo.id].id + '/350/250';
+            //         photo.author = photos[photo.id].author;
+            //         photo.unsplashUrl = photos[photo.id].url;
+            //     });
+            // });
+
+            this.photos.forEach(photo => {
+                photo.src = './photo.jpg';
+                photo.author = 'Fulano de Tal';
+                photo.unsplashUrl = 'https://unsplash.com/photos/nDqA4d5NL0k';
+            });            
         }
     }
 });
